7fe744922286035710076b52e2c1631744b53f17
Jesper Berth <jesper.berth@arrow.com>
2019-10-02 09:18:43 +0200

    setup_users

--- setup_class/azure_class_playbooks/setup_users.ps1
+++ setup_class/azure_class_playbooks/setup_users.ps1
@@ -8,12 +8,12 @@ function CreateUser {
     write-host $emailString
     $rgname = $emailString
     
-    $uniqtext1 = 1..99 | Get-Random -Count 1
-    $uniqtext2 = 1..99 | Get-Random -Count 1
-    $sec = Get-Date -Second
-    write-host $uniqtext2
-    write-host $sec
-    $uniqname = 
+    #$uniqtext1 = 1..99 | Get-Random -Count 1
+    #$uniqtext2 = 1..99 | Get-Random -Count 1
+    #$sec = Get-Date -Second
+    #write-host $uniqtext2
+    #write-host $sec
+    #$uniqname = 
 
     New-AzureADMSInvitation -InvitedUserDisplayName $name -InvitedUserEmailAddress $email -InviteRedirectURL https://portal.azure.com -SendInvitationMessage $true
     New-AzureRmResourceGroup -Name $rgname -Location $location
@@ -23,7 +23,7 @@ function CreateUser {
 
     New-AzureRmRoleAssignment -ObjectId $group.ObjectId -RoleDefinitionName Contributor -ResourceGroupName $rgname
 
-    New-AzStorageAccount -Name $uniqname -ResourceGroupName $rgname -Location $location -SkuName Standard_LRS -Kind StorageV2
+    #New-AzStorageAccount -Name $uniqname -ResourceGroupName $rgname -Location $location -SkuName Standard_LRS -Kind StorageV2
 }
 $email = Read-Host -Prompt 'Input email'
 $name = = Read-Host -Prompt 'Input name'
